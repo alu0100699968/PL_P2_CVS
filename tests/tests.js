13c5b149bd103cd5c657877b7edcbee6849413d3
alu0100696455 <alu0100696455@ull.edu.es>
2015-03-11 21:40:26 +0000

    Cambios en tests.js

--- tests/tests.js
+++ tests/tests.js
@@ -6,19 +6,19 @@ suite('csv', function() {
     if (typeof __html__ !== 'undefined') {
       document.body.innerHTML = __html__['tests/index.html'];
       original = document.getElementById('original');
-      result = document.getElementById('final');
+      finaltable = document.getElementById('finaltable');
     }
   });
 
   test('Dos filas con tres columnas', function() {
     original.value = "a, 1, 2 \n b, 3, 4";
     calculate();
-    assert.deepEqual(result.innerHTML, "<table class="center" id="result"><tbody><tr><td>a</td><td> 1</td><td> 2</td></tr><tr><td>b</td><td> 3</td><td> 4</td></tr></tbody></table>");
+    assert.deepEqual(finaltable.innerHTML, '<table class="center" id="result"><tbody><tr><td>a</td><td> 1</td><td> 2</td></tr><tr><td>b</td><td> 3</td><td> 4</td></tr></tbody></table>');
   });
   
   test('Dos filas con distinto número de columnas', function() {
     original.value = "a, 1, 2 \n b, 3, 4, 5";
     calculate();
-    assert.match(result.innerHTML, "<table class="center" id="result"><tbody><tr><td>a</td><td> 1</td><td> 2</td></tr><tr class="error"><td>b</td><td> 3</td><td> 4</td><td> 5</td></tr></tbody></table>");
+    assert.match(finaltable.innerHTML, 'class="error"');
   });
 });
