ed5056eabdadb977d5efb4415cd89828cb4d3293
alu0100696455 <alu0100696455@ull.edu.es>
2015-03-18 22:49:44 +0000

    Cambios en csv.js

--- csv.js
+++ csv.js
@@ -3,7 +3,11 @@
 
 $(document).ready(function() {
    $("button").click(function() {
-     calculate();
+      if (window.localStorage) localStorage.original = document.getElementById("original").value;
+         $.get("/commaSeparated", { input: document.getElementById("original").value }, function (data) {
+         var table = document.getElementById("template").innerHTML;
+         $("#finaltable").html(_.template(table, { rows: data.rows }));
+      }, 'json');
    });
  });
 
