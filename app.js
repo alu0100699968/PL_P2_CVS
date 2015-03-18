3154bc00aa641228d8be385ced25ee0ad77c4ad0
alu0100696455 <alu0100696455@ull.edu.es>
2015-03-18 21:49:21 +0000

    Añadiendo script para el template

--- app.js
+++ app.js
@@ -1,40 +1,70 @@
+var _ = require('underscore');
+var $ = require('jquery');
 var express = require('express');
 var app = express();
 var path = require('path');
 var expressLayouts = require('express-ejs-layouts');
 
-app.set('port', (process.env.PORT || 3000));
 
-// view engine setup
+app.set('port', (process.env.PORT || 7140));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-//app.set('layout', 'myLayout') // defaults to 'layout'  '
-
 app.use(expressLayouts);
-//app.use(express.static('public'));
-app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (req, res) {
-  res.render('index', { title: 'Express' });
-})
+app.use(express.static(__dirname + '/'));
 
-app.get('/chuchu', function (req, res) {
-  var isAjaxRequest = req.xhr;
-  console.log(isAjaxRequest);
-  if (isAjaxRequest) {
-    console.log(req.query);
-    res.send('{"answer": "Server responds: hello world!"}')
-  }
-  else {
-    res.send('not an ajax request');
-  }
+app.get('/', function (request, response) {     
+    response.render('index', { title: 'Analizador CSV' });
 });
 
-var server = app.listen(app.get('port'), function () {
+app.get('/commaSeparated', function (request, response) {
 
-  var host = server.address().address
-  var port = server.address().port
+    var result;
+    var original = request.query.input
+    var temp = original;
+    var regexp = /\s*"((?:[^"\\]|\\.)*)"\s*,?|\s*([^,]+),?|\s*,/g;
+    var lines = temp.split(/\n+\s*/);
+    var commonLength = NaN;
+    var row;
+    var rows = [];
+   
+    for (var t in lines) {
+        var temp = lines[t];
+        var m = temp.match(regexp);
+        var result = [];
+        var error = false;
 
-  console.log('Example app listening at http://%s:%s', host, port)
+        if (m) {
+            if (commonLength && (commonLength != m.length)) {
+                //alert('ERROR! row <'+temp+'> has '+m.length+' items!');
+                error = true;
+            }
+            else {
+                commonLength = m.length;
+                error = false;
+            }
+            for (var i in m) {
+                var removecomma = m[i].replace(/,\s*$/, '');
+                var remove1stquote = removecomma.replace(/^\s*"/, '');
+                var removelastquote = remove1stquote.replace(/"\s*$/, '');
+                var removeescapedquotes = removelastquote.replace(/\\"/, '"');
+                result.push(removeescapedquotes);
+            }
+            var tr = error? 'class="error">' : '';
+            row = new Object();
+            row.type = tr;
+            row.items = result;
+            rows.push(row);
+        }
+        else {
+            alert('ERROR! row ' + temp + ' does not look as legal CSV');
+            error = true;
+        }
+    }
+    response.send({ "rows": rows });
+});
 
+app.listen(app.get('port'), function () {
+    console.log("Node app is running at localhost:" + app.get('port'));
 });
