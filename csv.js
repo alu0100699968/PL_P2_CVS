c259ded535da1037e9deef6d5c4997b3529806ea
alu0100696455 <alu0100696455@ull.edu.es>
2015-03-18 23:21:26 +0000

    Arreglado fallo en el script del template

--- csv.js
+++ csv.js
@@ -4,7 +4,7 @@
 $(document).ready(function() {
    $("button").click(function() {
       if (window.localStorage) localStorage.original = document.getElementById("original").value;
-         $.get("/commaSeparated", { input: document.getElementById("original").value }, function (data) {
+      $.get("/commaSeparated", { input: document.getElementById("original").value }, function (data) {
          var table = document.getElementById("template").innerHTML;
          $("#finaltable").html(_.template(table, { rows: data.rows }));
       }, 'json');
