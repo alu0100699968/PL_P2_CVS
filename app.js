48cb1501c4fb513a706f132af43db00fa2c54dad
alu0100696455 <alu0100696455@ull.edu.es>
2015-03-18 20:37:17 +0000

    Cambios en app.js

--- app.js
+++ app.js
@@ -1,60 +1,40 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
+var path = require('path');
+var expressLayouts = require('express-ejs-layouts');
+
+app.set('port', (process.env.PORT || 3000));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+//app.set('layout', 'myLayout') // defaults to 'layout'  '
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressLayouts);
+//app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
-app.use('/', routes);
-app.use('/users', users);
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Express' });
+})
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.get('/chuchu', function (req, res) {
+  var isAjaxRequest = req.xhr;
+  console.log(isAjaxRequest);
+  if (isAjaxRequest) {
+    console.log(req.query);
+    res.send('{"answer": "Server responds: hello world!"}')
+  }
+  else {
+    res.send('not an ajax request');
+  }
 });
 
-// error handlers
+var server = app.listen(app.get('port'), function () {
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
+  var host = server.address().address
+  var port = server.address().port
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  console.log('Example app listening at http://%s:%s', host, port)
+
 });
-
-
-module.exports = app;
